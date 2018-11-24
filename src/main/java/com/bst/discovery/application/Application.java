2bca182070c10dbba37deadef0a608f4a68a6f17
ashwin <ashwinkn@hotmail.com>
2018-11-23 23:55:35 -0600

    Expose config refresh endpoint

--- src/main/java/com/bst/discovery/application/Application.java
+++ src/main/java/com/bst/discovery/application/Application.java
@@ -2,10 +2,12 @@ package com.bst.discovery.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RefreshScope
 public class Application {
 
 	public static void main(String[] args) {
